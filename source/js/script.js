3fabb4af6c7ffb83041eb853506f0c8daabcf391
Kiriuha-lucky <42931612+Kiriuha-lucky@users.noreply.github.com>
2020-05-06 22:11:48 +1200

    add

--- source/js/script.js
+++ source/js/script.js
@@ -12,6 +12,7 @@
     let $boardWidth = 1.380;
     let $transferX = 0;
     let $transferY = 0;
+    let $boardsCount = 0;
 
     //Считаем площадь пола и площадь стен
     let countFloorArea = function (w, l) {
@@ -21,29 +22,46 @@
         return ((w + l) * 2) * h;
     };
 
+    let boardsDraw = function (corX, corY) {
+        $('canvas').drawRect({
+            fillStyle: '#000',
+            strokeWidth: 1,
+            strokeStyle: 'red',
+            x: corX, y: corY,
+            fromCenter: false,
+            width: $boardWidth * $transferX,
+            height: $boardHeight * $transferY
+        });
+    };
+
 
     $('.parameters').change(function () {
         $roomWidth = parseInt($('#room_width').val(), 10);
         $roomLength = parseInt($('#room_length').val(), 10);
         $roomHeight = parseInt($('#room_height').val(), 10);
-
+        $boardsCount = Math.ceil(($roomLength * $roomWidth) / ($boardHeight * $boardWidth));
         $transferX = ($canvasWidth - ($canvasWidth * 0.2)) / $roomLength;
         $transferY = ($canvasHeight - ($canvasHeight * 0.2)) / $roomWidth;
+        // $('canvas').clearCanvas();
+
+        console.log($boardsCount);
+
+        for (let i = 0; i < $boardsCount; i++) {
+            console.log(555);
+            boardsDraw(($startPointX + ($boardWidth * $transferX * i)), $startPointY);
+        }
+        ;
 
 
-
-        $('canvas').clearCanvas();
-        console.log($startPointX);
-        console.log($startPointY);
-        $('canvas').drawRect({
-            fillStyle: '#000',
-            strokeWidth: 1,
-            strokeStyle: 'red',
-            x: $startPointX, y: $startPointY,
-            fromCenter: false,
-            width: $boardWidth * $transferX,
-            height: $boardHeight * $transferY
-        });
+        // $('canvas').drawRect({
+        //     fillStyle: '#000',
+        //     strokeWidth: 1,
+        //     strokeStyle: 'red',
+        //     x: $startPointX, y: $startPointY,
+        //     fromCenter: false,
+        //     width: $boardWidth * $transferX,
+        //     height: $boardHeight * $transferY
+        // });
         console.log($startPointX);
         console.log($startPointY);
         $('.room_area').text(countFloorArea($roomWidth, $roomLength));
